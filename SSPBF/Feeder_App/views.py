e24e328f0fbb1f5b7507c1ad99b017f2a1ce47ac
xueytian <xueytian@gmail.com>
2021-03-28 16:05:53 +0800

    add two pages

--- SSPBF/Feeder_App/views.py
+++ SSPBF/Feeder_App/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import image
+from .models import image, food
 
 def index(request):
     pic_list = image.objects.all()
-    print("..................................")
-    print(pic_list[0].image.url)
-    print(pic_list[0].image.path)
     return render(request, 'pic.html', {'pic_list': pic_list})
+
+def message(request):
+    message_list_unread = food.objects.filter(check=False).order_by('-notice_date')
+    message_list_read = food.objects.filter(check=True).order_by('-notice_date')
+    for message in message_list_unread:
+        message.check = True
+        message.save()
+    return render(request, 'message.html', {'message_list_unread': message_list_unread, 'message_list_read': message_list_read})
--- SSPBF/SSPBF/urls.py
+++ SSPBF/SSPBF/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('Feeder_App/', include('Feeder_App.urls')),
+    path('', include('Feeder_App.urls')),
     path('admin/', admin.site.urls),
 ]
 
