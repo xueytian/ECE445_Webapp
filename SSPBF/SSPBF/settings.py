38063d74955b0203ce27b4a4b9b34bc1bb1e8989
xueytian <xueytian@gmail.com>
2021-03-29 14:30:53 +0800

    minor change

--- SSPBF/SSPBF/settings.py
+++ SSPBF/SSPBF/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
